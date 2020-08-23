c49cfff25d911982f4d7a8b2e00d680347db1f66
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-23 12:10:28 +0300

    Lesson 2 - base template added - includes added for catalog, navigation and cart - dynamic title added

--- kwshop/main/views.py
+++ kwshop/main/views.py
@@ -4,17 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    print('From index.')
-    return render(request, 'main/index.html')
+    context = {
+        'page_title': 'Главная'
+    }
+    return render(request, 'main/index.html', context = context)
 
 def catalog(request):
-    print('From catalog.')
-    return render(request, 'main/catalog.html')
+    context = {
+        'page_title': 'Каталог'
+    }
+    return render(request, 'main/catalog.html', context = context)
 
 def cart(request):
-    print('From cart.')
-    return render(request, 'main/cart.html')
+    context = {
+        'page_title': 'Корзина'
+    }
+    return render(request, 'main/cart.html', context = context)
 
 def contact(request):
-    print('From contact.')
-    return render(request, 'main/contact.html')
+    context = {
+        'page_title': 'Обратная связь'
+    }
+    return render(request, 'main/contact.html', context = context)
