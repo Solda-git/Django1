8b74eccd5cfba566b9f3ddeb1c4d21500dba4c52
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-23 12:55:29 +0300

    Lesson 2 - base template added - includes added for catalog, navigation and cart - dynamic title added - dynamic static behavior added

--- kwshop/main/views.py
+++ kwshop/main/views.py
@@ -5,24 +5,24 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'page_title': 'Главная'
+        'page_title': 'главная'
     }
     return render(request, 'main/index.html', context = context)
 
 def catalog(request):
     context = {
-        'page_title': 'Каталог'
+        'page_title': 'каталог'
     }
     return render(request, 'main/catalog.html', context = context)
 
 def cart(request):
     context = {
-        'page_title': 'Корзина'
+        'page_title': 'корзина'
     }
     return render(request, 'main/cart.html', context = context)
 
 def contact(request):
     context = {
-        'page_title': 'Обратная связь'
+        'page_title': 'обратная связь'
     }
     return render(request, 'main/contact.html', context = context)
