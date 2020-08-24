196a863b56694a3ee00a5624e2b506872754da28
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-25 01:24:49 +0300

    Lesson 3 - local urls routing added - models, migrations added

--- kwshop/main/models.py
+++ kwshop/main/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    pass
 
 class ProductCat(models.Model):
-    title = models.CharField(max_length=25, verbose_name='Категория')
+    title = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCat,
+                                 on_delete=models.PROTECT,
+                                 verbose_name='Категория')
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2, default=0)
+    img = models.ImageField(upload_to='product_imgs', blank=True)
+    size = models.CharField(max_length=50, verbose_name='Размер', default='см')
+    stuff = models.CharField(max_length=100, verbose_name='Материал', default='акриловые нитки, холофайбер')
+    description = models.TextField(verbose_name='Описание', blank=True)
 
 
+    def __str__(self):
+        return f'{self.name}, категория: {self.category.title}'
