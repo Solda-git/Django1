5ff186dcd37e47df5e7662d9d5a28500758f8160
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-25 02:22:56 +0300

    Lesson 3 - local urls routing added - models, migrations added - admin role created - dump file added, .bat script worked out (dump restore added)

--- kwshop/run.bat
+++ kwshop/run.bat
@@ -1,2 +1,3 @@
-..\venv\scripts\activate & python manage.py runserver 192.168.1.195:8000
+..\venv\scripts\activate & python manage.py migrate & python manage.py loaddata maindump.json & python manage.py runserver 192.168.1.195:8000
+
 pause
