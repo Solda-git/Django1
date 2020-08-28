f91b2ef769b9d30f55896cbbac61b9efc0a8a8ac
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-28 23:54:27 +0300

    Lesson 4 - knitted shop authorisation application (kwauth) added - KWUser added - fill_db implemented - login form & controller added - authenticate added

--- kwshop/kwauth/views.py
+++ kwshop/kwauth/views.py
@@ -1,17 +1,29 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse
 from kwauth.forms import KWAuthenticationForm
 
 
 def login(request):
-    form = KWAuthenticationForm()
-    context = {
-        'title': 'аутентификация',
-        'form': form
-    }
-    return render(request, 'kwauth/login.html', context)
+    form = None
+    if request.method == 'POST':
+        form = KWAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('main:index'))
+    elif request.method == 'GET':
+        form = KWAuthenticationForm()
+        context = {
+            'title': 'аутентификация',
+            'form': form
+        }
+        return render(request, 'kwauth/login.html', context)
 
 def logout(request):
     pass
