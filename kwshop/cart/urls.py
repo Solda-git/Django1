dba643fa0e8934129c9e29f0c5a9286d22750421
Sergey Soldatov <soldatovsl@mail.ru>
2020-09-01 10:00:35 +0300

    Lesson 5 - cart application created - dynamic product menu with products filtering added - add_item added within cart application

--- kwshop/cart/urls.py
+++ kwshop/cart/urls.py
@@ -2,13 +2,14 @@
     main application
 """
 
-from django.urls import path
+from django.urls import path, re_path
 import cart.views as cart
 
 app_name = 'kwauth'
 
 urlpatterns = [
     path('', cart.index, name='index'),
+    re_path(r'^add/product/(?P<pk>\d+)/$', cart.add_item, name='add_item'),
 
 ]
 
--- kwshop/cart/views.py
+++ kwshop/cart/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from cart.models import CartItem
+from main.models import Product
+
 
 def index(request):
     pass
+
+def add_item(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    cart = CartItem.objects.filter(user=request.user, product=product).first()
+    if not cart:
+        cart = CartItem(user=request.user, product=product)
+    cart.quantity += 1
+    cart.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
