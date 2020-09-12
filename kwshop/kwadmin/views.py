4679136f2d3d7dd3059386c25e206d0b97a9e9a1
Sergey Soldatov <soldatovsl@mail.ru>
2020-09-12 18:58:20 +0300

    Lesson 7: - next added (login/logout redirect) - ajax communication implemented. add and delete methods within the cart box implemented - kwadmin application added, index.html added (user list implemented)

--- kwshop/kwadmin/views.py
+++ kwshop/kwadmin/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
 
 @user_passes_test (lambda u: u.is_superuser)
 def index(request):
-    pass
+    object_list = get_user_model().objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+    context = {
+        'title': 'пользователи',
+        'object_list': object_list,
+    }
+    return render(request, 'kwadmin/index.html', context)
