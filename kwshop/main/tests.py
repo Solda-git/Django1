dbaeed84bceb7d4471559cdb1d7a53c739c8e44a
solda <soldatovsl@mail.ru>
2020-10-15 09:33:47 -0700

    Lesson8 - F-object added to cart:add controller (check added) - Bug "active product cat - active products" fixed - product category discount implemented - conditional-expressions added (learn_db.py)
    - starting with test.py

--- kwshop/main/tests.py
+++ kwshop/main/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.core.management import call_command
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+class TestMainappSmoke(TestCase):
+   def setUp(self):
+        call_command('flush', '--noinput')
+        call_command('loaddata', 'test_db.json')
+        self.client = Client()
