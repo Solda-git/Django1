5982baeada3f8427a037b7741a28f6df5e112de3
solda <soldatovsl@mail.ru>
2020-10-18 09:35:05 -0700

    Lesson8 - F-object added to cart:add controller (check added) - Bug "active product cat - active products" fixed - product category discount implemented - conditional-expressions added (learn_db.py)
    - starting with test.py
    - smoketest fpo main appllication added
    - login test added
    - test_cart_login_redirect added

--- kwshop/kwauth/tests/tests.py
+++ kwshop/kwauth/tests/tests.py
@@ -1,3 +1,4 @@
+from django.core.management import call_command
 from django.test import TestCase, Client
 
 
@@ -7,21 +8,24 @@ from kwauth.models import KWUser
 
 class TestUserManagement(TestCase):
 
-    #fixtures = ['kwauth.json', 'main.json', 'cart.json', 'orders.json']
+    # fixtures = ['kwauth.json', 'main.json', 'cart.json', 'orders.json']
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.client = Client()
-   #
-   # def setUp(self):
-   #     self.client = Client()
-        cls.superuser = KWUser.objects.create_superuser('django2','django2@geekshop.local', 'geekbrains')
-        cls.user = KWUser.objects.create_user('solda2','solda2@geekshop.local', 'geekbrains')
-        cls.user_with__first_name = KWUser.objects.create_user('knitted', 'knitted@geekshop.local', 'geekbrains', first_name='Knitted world')
+        #
+        # def setUp(self):
+        #     self.client = Client()
+        cls.superuser = KWUser.objects.create_superuser('django2', 'django2@geekshop.local', 'geekbrains')
+        cls.user = KWUser.objects.create_user('solda2', 'solda2@geekshop.local', 'geekbrains')
+        cls.user_with__first_name = KWUser.objects.create_user('knitted', 'knitted@geekshop.local', 'geekbrains',
+                                                               first_name='Knitted world')
+
 
     def test_user_login(self):
         # главная без логина
+        print('test_user_login')
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
@@ -29,25 +33,35 @@ class TestUserManagement(TestCase):
         self.assertNotContains(response, 'Пользователь', status_code=200)
         # print(response.content.decode('utf-8'))
         self.assertNotIn('Пользователь', response.content.decode('utf-8'))
-        self.client.login(username='solda2', password='geekbrains')
-
-        # self.client.post('/auth/login/',
-        #                  data={
-        #                       'username': 'sodla2',
-        #                       'password': 'geekbrains'
-        #                  }
-        #                 )
+        # self.client.login(username='solda2', password='geekbrains')
+        print('<><><><><><><><><><><><><>')
+        self.client.post('/auth/login/',
+                         data={
+                              'username': 'solda2',
+                              'password': 'geekbrains'
+                         }
+                        )
         # # главная после логина
         response = self.client.get('/')
-        # print('**************')
-        # print(response.context.user)
-        # print('**************')
-        print(response.content.decode('utf-8'))
         self.assertContains(response, 'Пользователь', status_code=200)
-
         self.assertEqual(response.context['user'], self.user)
-        # self.assertIn('Пользователь', response.content.decode())
-   #
-   # def tearDown(self):
-   #     call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', \
-   #                  'basketapp')
+
+    def test_cart_login_redirect(self):
+        # без логина должен переадресовать
+        response = self.client.get('/cart/')
+        self.assertEqual(response.url, '/auth/login/?next=/cart/')
+        self.assertEqual(response.status_code, 302)
+
+        # с логином все должно быть хорошо
+        self.client.login(username='knitted', password='geekbrains')
+
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(list(response.context['cart']), [])
+        self.assertEqual(response.request['PATH_INFO'], '/cart/')
+        print(response.content.decode())
+        self.assertIn('КОРЗИНА', response.content.decode('utf-8'))
+
+    #
+    # def tearDown(self):
+    #     call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
--- kwshop/kwshop/settings.py
+++ kwshop/kwshop/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's-#ds@x#wlr##6fm$okfio6qt2#ghm_m1+d&u5z_$hyqz4sbc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -246,3 +248,4 @@ LOW_CACHE = True
 
 #Paging
 PRODUCTS_ON_PAGE_NUMBER = 4
+
